2784ec8af9bdf023a9e3c737a5fcc3b1b252c073
Srinivas Ankalap <2019205@iiitdmj.ac.in>
2023-05-08 16:48:24 +0530

    available

---
+++ src/components/allcourses/OnlineCourses.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import "./courses.css"
+
+const OnlineCourses = () => {
+  return (
+    <>
+      <section className='online'>
+        <div className='container'>
+          
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default OnlineCourses
---
+++ src/dummydata.js
@@ -0,0 +1,312 @@
+export const homeAbout = [
+  {
+    id: 1,
+    cover: "https://img.icons8.com/dotty/80/000000/storytelling.png",
+    title: "Online Courses",
+    desc: " It offers flexibility and convenience since learners can access course materials at their own pace and time.",
+  },
+  {
+    id: 1,
+    cover: "https://img.icons8.com/ios/80/000000/diploma.png",
+    title: "Earn A Certificates",
+    desc: "Earning a certificate through online learning expertise can improve one's career prospects and increase earning potential, demonstrating to employers a commitment to ongoing professional development.",
+  },
+  {
+    id: 1,
+    cover: "https://img.icons8.com/ios/80/000000/athlete.png",
+    title: "Learn with Expert",
+    desc: "Learning with experts through online learning expertise can enhance one's knowledge, skills, and professional opportunities.",
+  },
+]
+export const awrapper = [
+  {
+    cover: "https://img.icons8.com/external-yogi-aprelliyanto-basic-outline-yogi-aprelliyanto/80/ffffff/external-graduation-education-yogi-aprelliyanto-basic-outline-yogi-aprelliyanto.png",
+    data: "500+",
+    title: "SUCCESS STORIES",
+  },
+
+  {
+    cover: "https://img.icons8.com/ios/80/ffffff/athlete.png",
+    data: "17",
+    title: "TRUSTED TUTORS",
+  },
+  {
+    cover: "https://img.icons8.com/external-outline-icons-maxicons/80/ffffff/external-calender-insurance-outline-outline-icons-maxicons.png",
+    data: "55",
+    title: "SCHEDULES",
+  },
+  {
+    cover: "https://img.icons8.com/ios/80/ffffff/macbook-idea--v3.png",
+    data: "9",
+    title: "COURSES",
+  },
+]
+export const coursesCard = [
+  {
+    id: 1,
+    cover: "../images/courses/c1.png",
+    coursesName: "Introducing to Software Engineering",
+    courTeacher: [
+      {
+        dcover: "/images/pic.jpg",
+        name: "by John Smith",
+        totalTime: "50 lectures (190 hrs)",
+      },
+    ],
+    priceAll: "Rs.100 All Course",
+    pricePer: "Rs.15 per month",
+  },
+  {
+    id: 2,
+    cover: "../images/courses/c2.png",
+    coursesName: "Enhancing Adobe Photoshop CC 2020 Skills",
+    courTeacher: [
+      {
+        dcover: "/images/2.jpg",
+        name: "by Ram Gurung",
+        totalTime: "30 lectures (125 hrs)",
+      },
+    ],
+    priceAll: "Rs.200 All Course",
+    pricePer: "Rs.25 per month",
+  },
+  {
+    id: 3,
+    cover: "../images/courses/c3.png",
+    coursesName: "HTML, CSS, and Javascript for Web Developers",
+    courTeacher: [
+      {
+        dcover: "/images/3.jpg",
+        name: "by Saroj Nepal",
+        totalTime: "50 lectures (50 hrs)",
+      },
+    ],
+    priceAll: "Rs.50 All Course",
+    pricePer: "Rs.5 per month",
+  },
+  {
+    id: 4,
+    cover: "../images/courses/c4.png",
+    coursesName: "Introducing to Programming with WordPress",
+    courTeacher: [
+      {
+        dcover: "/images/4.jpg",
+        name: "by Prabin Thapa Mager",
+        totalTime: "20 lectures (20 hrs)",
+      },
+    ],
+    priceAll: "Rs.30 All Course",
+    pricePer: "Rs.3 per month",
+  },
+  {
+    id: 5,
+    cover: "../images/courses/c5.png",
+    coursesName: "Introducing to Programming with ReactJS",
+    courTeacher: [
+      {
+        dcover: "/images/5.jpg",
+        name: "by Sunil Bishowkaram",
+        totalTime: "100 lectures (150 hrs)",
+      },
+    ],
+    priceAll: "Rs.300 All Course",
+    pricePer: "Rs.30 per month",
+  },
+  {
+    id: 6,
+    cover: "../images/courses/c6.png",
+    coursesName: "Learn Frontend Programming Language",
+    courTeacher: [
+      {
+        dcover: "/images/6.jpg",
+        name: "by Anil K Gourishetty",
+        totalTime: "200 lectures (300 hrs)",
+      },
+    ],
+    priceAll: "Rs.500 All Course",
+    pricePer: "Rs.80 per month",
+  },
+  {
+    id: 7,
+    cover: "../images/courses/c7.png",
+    coursesName: "Introducing to with HTML / CSS",
+    courTeacher: [
+      {
+        dcover: "/images/7.jpg",
+        name: "by Premananda Bera",
+        totalTime: "20 lectures (50 hrs)",
+      },
+    ],
+    priceAll: "Rs.20 All Course",
+    pricePer: "Rs.2 per month",
+  },
+  {
+    id: 8,
+    cover: "../images/courses/c8.png",
+    coursesName: "Introducing to with JAVA",
+    courTeacher: [
+      {
+        dcover: "/images/8.jpg",
+        name: "by Rajat Broch",
+        totalTime: "80 lectures (200 hrs)",
+      },
+    ],
+    priceAll: "Rs.200 All Course",
+    pricePer: "Rs.40 per month",
+  },
+  {
+    id: 9,
+    cover: "../images/courses/c9.png",
+    coursesName: "Introducing to with HTML / CSS / JAVASCRIPT",
+    courTeacher: [
+      {
+        dcover: "/images/9.jpg",
+        name: "by Abhijit Deuskar",
+        totalTime: "50 lectures (50 hrs)",
+      },
+    ],
+    priceAll: "Rs.20 All Course",
+    pricePer: "Rs.3 per month",
+  },
+]
+
+export const team = [
+  {
+    cover: "/images/blank.webp",
+    name: "HemaSagar",
+    work: "FOUNDER & CEO",
+  },
+  {
+    cover: "/images/blank.webp",
+    name: "Arshad K",
+    work: "CONSULTANT",
+  },
+  
+]
+export const price = [
+  {
+    name: "BASIC PLAN",
+    price: "4.9K",
+    desc: " The Basic Plan is designed for those who are just starting their learning journey and want to explore a wide range of topics at a low cost. This plan typically includes access to a limited number of courses and resources, such as videos and articles, with no additional features or benefits.",
+  },
+  {
+    name: "BEGINNER PLAN",
+    price: "7.9K",
+    desc: "The Beginner Plan is geared towards learners who are ready to commit to their education and want more comprehensive course offerings. This plan usually includes access to a broader range of courses, additional resources, and basic features such as discussion forums and quizzes. It's a great option for those who are just getting started and want to build a strong foundation in their chosen field.",
+  },
+  {
+    name: "PREMIUM PLAN",
+    price: "10.9k",
+    desc: "The Premium Plan is designed for more advanced learners who are seeking a deeper and more immersive learning experience. This plan typically includes access to a larger selection of courses and resources, as well as advanced features such as personalized coaching, virtual labs, and networking opportunities. The Premium Plan is ideal for learners who are looking to advance their careers or gain expertise in a specific area.",
+  },
+  {
+    name: "ULTIMATE PLAN",
+    price: "14.9K",
+    desc: "The Ultimate Plan is the most comprehensive and advanced plan, offering the highest level of access to courses, resources, and features. This plan usually includes everything offered in the Premium Plan, plus additional benefits such as exclusive content, mentorship programs, and certification programs. The Ultimate Plan is ideal for learners who are seeking the highest level of expertise and want to achieve the highest level of success in their field.",
+  },
+]
+export const faq = [
+  {
+    title: "What is an online learning platform?",
+    desc: "An online learning platform is a digital platform that provides learners with access to educational resources, courses, and training materials over the internet.",
+  },
+  {
+    title: "What kind of courses are available on the online learning platform?",
+    desc: " This online learning platforms offers a wide variety of courses across different fields, including business, technology, health, education, and more. These courses range from beginner to advanced levels and cover different topics and skills.",
+  },
+  {
+    title: "How do I access my courses on the online learning platform?",
+    desc: "Once you've signed up for a course, you'll be given access to the platform's learning management system, which will allow you to view course materials, complete assignments, and interact with other learners.",
+  },
+  {
+    title: "Can I interact with other learners on the platform?",
+    desc: "Yes, this online learning platform offers discussion forums, chat rooms, and other interactive features that allow learners to connect and collaborate with other students.",
+  },
+  {
+    title: "How long do I have access to my courses?",
+    desc: "The length of access to courses can vary by plan. Some plans offer lifetime access to courses, while others offer access for a set period of time (such as six months or one year).",
+  },
+  {
+    title: "What kind of support is available on the online learning platform?",
+    desc: "Our learning platform offers technical support, as well as access to instructors and mentors who can provide guidance and answer questions about course content.",
+  },
+]
+export const blog = [
+  {
+    id: 1,
+    type: "admin",
+    date: "2 weeks ago",
+    com: "3 COMMENTS ",
+    title: "Mastering the Art of Time Management: A Guide for Busy Professionals",
+    desc: "Learn how to manage your time effectively with our comprehensive guide, designed for busy professionals looking to balance work, study, and personal commitments.",
+    cover: "../images/blog/b1.webp",
+  },
+  {
+    id: 2,
+    type: "admin",
+    date: "11 weeks ago",
+    com: "5 COMMENTS ",
+    title: "The Future of Work: How to Stay Ahead of the Game with Online Learning",
+    desc: "Explore the latest trends in the ever-evolving world of work and discover how online learning can help you stay ahead of the curve and advance your career.",
+    cover: "../images/blog/b2.webp",
+  },
+  {
+    id: 3,
+    type: "user",
+    date: "1 week ago",
+    com: "10 COMMENTS ",
+    title: "From Novice to Pro: How to Master New Skills with Online Learning",
+    desc: "Discover the secrets to mastering new skills quickly and efficiently with online learning, no matter your current level of expertise.",
+    cover: "../images/blog/b3.webp",
+  },
+  {
+    id: 4,
+    type: "admin",
+    date: "20 days ago",
+    com: "3 COMMENTS ",
+    title: "Unlocking Your Creative Potential: How Online Learning Can Help You Spark Innovation",
+    desc: "Learn how to tap into your creative potential with the help of our online learning platform, designed to help you unleash your imagination and turn your ideas into reality.",
+    cover: "../images/blog/b4.webp",
+  },
+  {
+    id: 5,
+    type: "admin",
+    date: "8 weeks ago",
+    com: "3 COMMENTS ",
+    title: "The Power of Collaboration: How Online Learning Can Help You Build Professional Networks",
+    desc: "Discover the benefits of collaborating with other professionals in your field and learn how to build your own professional network with the help of our online learning platform.",
+    cover: "../images/blog/b5.webp",
+  },
+  {
+    id: 6,
+    type: "user",
+    date: "recent",
+    com: "12 COMMENTS ",
+    title: "The Benefits of Lifelong Learning: How Online Learning Can Help You Thrive in Any Industry",
+    desc: "Explore the many benefits of lifelong learning and discover how online learning can help you stay relevant and adaptable in any industry, no matter your age or experience level.",
+    cover: "../images/blog/b6.webp",
+  },
+]
+export const testimonal = [
+  {
+    id: 1,
+    name: "UDAYA ABHISHEK",
+    post: "Marketing Manager",
+    desc: "I have had an amazing experience with this online learning platform. The courses are well-structured and easy to follow, and the instructors are knowledgeable and engaging. I especially appreciate the flexibility that online learning provides, allowing me to balance my studies with work and other commitments.",
+    cover: "/images/r1.jpg",
+  },
+  {
+    id: 2,
+    name: "VINESH CHARAN",
+    post: "Sr.Developer",
+    desc: "I have been using this online learning platform for over a year now and I can honestly say that it has transformed my life. As a busy professional, I struggled to find the time and resources to pursue my personal and professional goals. But with the help of this platform, I have been able to access a wealth of knowledge and expertise from leading experts in my field, on my own schedule.",
+    cover: "/images/r2.jpg",
+  },
+  {
+    id: 3,
+    name: "AYUSH VELLANKI",
+    post: "Software Engineer",
+    desc: "I was initially skeptical about the effectiveness of online learning, but this platform has completely changed my mind. The courses are designed with the learner in mind and are taught by experts in their respective fields. The platform is user-friendly and easy to navigate, making it simple to access course materials and track my progress.",
+    cover: "/images/r3.jpg",
+  },
+]
